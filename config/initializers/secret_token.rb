7c4a684fe73a1b3f456db9fdc033af37022205e5
Jacob Ashdown <jcbashdown@gmail.com>
2011-10-24 20:54:02 +0100

    rspec, cucumber and devise to new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Centre::Application.config.secret_token = '1853c2bfde0951b454de209a11b4b6f1651b65bbd12452fd85e1e4aa8340d5d734eeb5a60338e42de92fe47f4e57c679065a14360ab11ec1807c13672c10659b'
