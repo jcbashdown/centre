3af4aaca152d19b5100c41131b55b6c3a6714489
Jacob Ashdown <jcbashdown@gmail.com>
2011-11-30 00:35:15 +0000

    commit some nonsense - could be rubbish - links form

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,4 +1,9 @@
 class Link < ActiveRecord::Base
   belongs_to :source_node, :foreign_key => "node_from", :class_name => "Node"
   belongs_to :target_node, :foreign_key => "node_to", :class_name => "Node"
+  # do we definitely want to limit to having a support or oppose relation? What about undecided or related?
+  #spec this
+  validates :node_from, :presence => true
+  validates :node_to, :presence => true
+  
 end
--- app/models/node.rb
+++ app/models/node.rb
@@ -4,6 +4,16 @@ class Node < ActiveRecord::Base
   has_many :links_to, :foreign_key => "node_from", :class_name => "Link"
   has_many :target_nodes, :through => :links_to, :class_name => "Node", :foreign_key => "node_from"
   has_many :source_nodes, :through => :links_in, :class_name => "Node", :foreign_key => "node_to"
+  
+  accepts_nested_attributes_for :links_in, :links_to, :reject_if => :reject_link
   #has_many :related_nodes, :through => :links
+  #spec this - has many so this is wrong approach?
+  def reject_link(hash)
+    hash.each do |key|
+      unless key=='user_id'
+        return hash[key].blank?
+      end
+    end 
+  end
   
 end
