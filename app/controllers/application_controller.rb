a27dc56170362fe9c58c057607009d949b885a78
Jacob Ashdown <jcbashdown@googlemail.com>
2012-01-19 23:44:02 +0000

    get current user before filter and some form twiddling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_filter :set_current_user
   protect_from_forgery
   include FrontendHelpers::Html5Helper
+
+  def set_current_user
+    @user = current_user
+    p @user
+  end
+
 end
