a15f1b3a0a83359039bdbfd2615519df6c894914
Jacob Ashdown <jcbashdown@googlemail.com>
2012-01-26 23:54:20 +0000

    some login bar business

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,5 +7,11 @@ class ApplicationController < ActionController::Base
   def set_current_user
     @user = current_user
   end
+  
+  def set_user_session
+    unless current_user
+      session = UserSession.build  
+    end
+  end
 
 end
