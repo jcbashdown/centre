6ab2383b39ca5adc9316aa99357b49db36a3cbbc
Jacob Ashdown <jcbashdown@googlemail.com>
2012-05-15 00:14:53 +0100

    fix the getting rid of positive and negative tags unless added refactor

--- app/models/global_node_user.rb
+++ app/models/global_node_user.rb
@@ -31,25 +31,25 @@ class GlobalNodeUser < ActiveRecord::Base
       this_negative_node_argument_doc = Nokogiri::XML(%Q|<negative>|+this_negative_node_argument.content+%Q|</negative>|) do |config|
         config.default_xml.noblanks
       end
-      this_negative_node_argument = this_negative_node_argument_doc.xpath('/negative').first
+      this_negative_node_argument_content = this_negative_node_argument_doc.xpath('/negative').first
     end
     this_positive_node_argument = PositiveNodeArgument.select(:content).where(:subject_id => self.id, :subject_type => 'GlobalNodeUser')[0]
     if this_negative_node_argument.content.length > 0
       this_positive_node_argument_doc = Nokogiri::XML(%Q|<positive>|+this_positive_node_argument.content+%Q|</positive>|) do |config|
         config.default_xml.noblanks
       end
-      this_positive_node_argument = this_positive_node_argument_doc.xpath('/positive').first
+      this_positive_node_argument_content = this_positive_node_argument_doc.xpath('/positive').first
     end
 
     to_doc = Nokogiri::XML(self.to_xml) do |config|
       config.default_xml.noblanks
     end
     to_node = to_doc.xpath('/global-node-user').first
-    if this_negative_node_argument 
-      this_negative_node_argument.parent = to_node
+    if this_negative_node_argument_content
+      this_negative_node_argument_content.parent = to_node
     end
-    if this_positive_node_argument
-      this_positive_node_argument.parent = to_node
+    if this_positive_node_argument_content
+      this_positive_node_argument_content.parent = to_node
     end
     to_doc.to_xml
   end
