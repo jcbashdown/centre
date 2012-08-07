2e2e3304ea3a0d04b4e6601c014be89cb883e7e1
Jacob Ashdown <jcbashdown@googlemail.com>
2012-08-07 23:11:14 +0100

    start set links, nodes and arg

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -84,11 +84,50 @@ describe ApplicationController do
         end
       end
       context 'and all settings are overridden' do
-
+        before do
+          @user_two = FactoryGirl.create(:user, :email => "new@test.com")
+          @question_two = FactoryGirl.create(:question, :name => "new name")
+          @existing_view_configuration = 
+                               {
+                                :nodes_question => @question.id,
+                                :nodes_user => @user.id,
+                                :nodes_query => "A node",
+                                :argument_user => @question.id,
+                                :argument_question => @user.id,
+                                :links_question => @question.id,
+                                :links_user => @user.id,
+                                :links_query => "A node"
+                               }
+          @new_view_configuration = 
+                               {
+                                :nodes_question => @question_two.id,
+                                :nodes_user => @user_two.id,
+                                :nodes_query => "Another node",
+                                :argument_user => @question_two.id,
+                                :argument_question => @user_two.id,
+                                :links_question => @question_two.id,
+                                :links_user => @user_two.id,
+                                :links_query => "Another node"
+                               }
+          @params = {:view_configuration => @new_view_configuration}
+          @existing_view_configuration.each do |key, value|
+            request.cookies[key] = value.to_s
+          end
+        end
+        it 'should override the existing configuration if overridden' do
+          get :index, @params
+          @new_view_configuration.each do |key, value|
+            request.cookie_jar[key.to_s].should == value.to_s
+          end
+        end
+        it 'should set the correct defaults when nothing is set' do
+          pending
+        end
       end
     end
     
     it 'it should set the correct expiry' do
+      #auto set to browser close apparently
       pending
     end
     it 'should be as secure as all the when stuff' do
@@ -97,6 +136,23 @@ describe ApplicationController do
     it 'should behave correctly when both a question and user set (if not allowed)' do
       pending
     end
+    it 'should decode the hashed params' do
+      pending
+    end
+  end
+  
+  describe 'set_nodes' do
+    context 'when the question is set' do
+
+    end
+  end
+
+  describe 'set_links' do
+
+  end
+
+  describe 'set_argument' do
+
   end
 
 end
