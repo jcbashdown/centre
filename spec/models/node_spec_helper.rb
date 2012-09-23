50e91849fabfe331901a041f0f7fb69ed8d337ec
Jacob Ashdown <jcbashdown@googlemail.com>
2012-09-23 22:40:02 +0100

    not saving, user and question nil?

--- spec/models/node_spec_helper.rb
+++ spec/models/node_spec_helper.rb
@@ -8,7 +8,7 @@ shared_examples_for 'a node finding directed links' do |direction, this_node|
             @queries.each do |query|
               if cn.title =~ Regexp.new(query)
                 @links[:"user#{iu}_question#{iq}_#{query}"] = 
-                  ContextLink::PositiveContextLink.create(:user=>user, :question => question, :"global_node_#{direction}_id" => cn.global_node.id, :"global_node_#{this_node}_id" => @node.id).global_link        
+                  ContextLink::PositiveContextLink.create!(:user=>user, :question => question, :"global_node_#{direction}_id" => cn.global_node.id, :"global_node_#{this_node}_id" => @node.id).global_link        
               end
             end
           end
@@ -18,9 +18,11 @@ shared_examples_for 'a node finding directed links' do |direction, this_node|
       @questions << nil
     end
     it 'should find the correct links for each context' do
+      p @links
       @users.each_with_index do |user, iu|
         @questions.each_with_index do |question, iq|
           @queries.each do |query|
+            p @node.find_view_links_by_context(direction, this_node, {:user => user.id, :question => question.id, :query => query})
             @node.find_view_links_by_context(direction, this_node, {:user => user.id, :question => question.id, :query => query}).should == [@links[:"user#{iu}_question#{iq}_#{query}"]]
           end
         end
