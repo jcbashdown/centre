5dc965d3a889552c782f873c5a18c3f827789a47
Jacob Ashdown <jcbashdown@googlemail.com>
2012-10-13 01:06:36 +0100

    rename globals controller to questions controllef

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,17 @@
+class QuestionsController < ApplicationController
+  def new
+    @question = Question.new
+    render :new, :layout => false
+  end
+
+  def create
+    @question = Question.new(params[:question])
+    if @question.save
+      flash[:notice]=@question.name+" created"
+    else
+      flash[:alert] = "Cannot create a blank question"
+    end
+    redirect_to nodes_path(:question=>@question.id)
+  end
+
+end
