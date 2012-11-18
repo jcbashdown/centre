85cfb7944db63d7cd4e82024a75f07fc22a92380
Jacob Ashdown <jcbashdown@googlemail.com>
2012-11-18 23:21:33 +0000

    more on args - start rels

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,7 +3,7 @@ class Question < ActiveRecord::Base
     :message => "Please use the pre defined 'All'" }
 
   def concluding_nodes
-    gn_ids = Node::QuestionNode.where(:question_id => self.id, :is_conclusion => true).collect(&:global_node_id)
-    Node::GlobalNode.where('id IN (?)', gn_ids)
+    Node::QuestionNode.where(:question_id => self.id, :is_conclusion => true)#.collect(&:global_node_id)
+    #Node::GlobalNode.where('id IN (?)', gn_ids)
   end
 end
