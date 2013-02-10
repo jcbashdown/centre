dbde5c4ef443b785527a4f2d42d6c7e3ffb32440
Jacob Ashdown <jcbashdown@googlemail.com>
2013-02-10 17:39:23 +0000

    delete conlusion store when appropriate

--- lib/conclusion.rb
+++ lib/conclusion.rb
@@ -9,11 +9,16 @@ module Conclusion
     is_conclusion_votes = Node.find_ids_by_context context.merge({:is_conclusion => true}) 
     if is_conclusion_votes > not_conclusion_votes
       find_or_create_conclusion context.merge({:global_node_id => global_node.id})
-    end 
+    else
+      destroy_conclusion_if_exists context.merge({:global_node_id => global_node.id})
+    end
   end
 
   def find_or_create_conclusion context
     where(context)[0] || create!(context)
   end
 
+  def destroy_conclusion_is_exists context
+    where(context)[0].try(:destroy)
+  end
 end
