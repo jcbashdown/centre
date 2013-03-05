36d0813bb5a78c458881b31fd5aa5b623bc7f898
jacob <jacob@jacob-1005HA.(none)>
2013-03-05 22:12:08 +0000

    test set context called

--- spec/lib/conclusion_spec_helper.rb
+++ spec/lib/conclusion_spec_helper.rb
@@ -1,12 +1,20 @@
 shared_examples_for "a conclusion class extending conclusion" do |special_context, inferred_context|
   
+  let(:context) {special_context ? {:silly => "context"}.merge(special_context) : {:silly => "context"}}
+
   describe ".update_conclusion_status_for" do
     before do
+      subject.stub(:set_context!)
+      subject.stub(:create_context)
       subject.stub(:votes_for_conclusion).and_return 0
       subject.stub(:votes_against_conclusion).and_return 0
       subject.stub(:find_or_create_conclusion)
       subject.stub(:destroy_conclusion_if_exists)
     end
+    it 'should call set_context! with context' do
+      subject.should_receive(:set_context!).with context
+      subject.update_conclusion_status_for context
+    end 
     context "when there are more votes for conclusion than against" do
       before do
         subject.stub(:votes_for_conclusion).and_return 1
@@ -30,7 +38,6 @@ shared_examples_for "a conclusion class extending conclusion" do |special_contex
     end
   end
   describe ".set_context" do
-    let(:context) {special_context ? {:silly => "context"}.merge(special_context) : {:silly => "context"}}
     it "should assign context to the class instance variable @context" do
       subject.send(:set_context!, context)
       subject.instance_variable_get("@context").should == (inferred_context ? context.merge(inferred_context) : context)
