7855d7f893011937e8f20e42816fd82483bf2b0e
Jacob Ashdown <jcbashdown@googlemail.com>
2013-03-17 12:27:54 +0000

    start to make argument showing through link search work

--- app/models/node/global_node.rb
+++ app/models/node/global_node.rb
@@ -18,19 +18,19 @@ class Node::GlobalNode < Node
 
   #alt would be find all links from ids of nodes, map ids of subset of nodes there are links for
   #from result, delete ids from nodes map and construct for rest, slightly more db efficient as group find?
-  def find_view_links_by_context direction, context
+  def find_view_links_by_context direction, context, type=Link
     nodes = self.class.find_by_context(context.except(:user_id, :page))
-    links = send(:"links_#{direction}_of", nodes, context.extract!(:user_id,:group_id))
+    links = send(:"links_#{direction}_of", nodes, context.extract!(:user_id,:group_id), type)
     Kaminari.paginate_array(links).page(context[:page]).per(10)
   end
 
   [:to, :from].each do |direction|
-    define_method :"links_#{direction}_of" do |nodes, context|
+    define_method :"links_#{direction}_of" do |nodes, context, type=Link|
       other_direction = Link.opposite_direction(direction)
       nodes.inject([]) do |links, node|
         unless node == self
           global_link_attrs = {:"global_node_#{direction}_id" => self.id, :"global_node_#{other_direction}_id" => node.id}
-          link = Link.where(global_link_attrs.merge(context))[0].try(:global_link)
+          link = type.where(global_link_attrs.merge(context))[0].try(:global_link)
           if link
             links << link
           else
