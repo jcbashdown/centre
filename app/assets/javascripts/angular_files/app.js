ead4f1fdeef47731762ded2b7d72744352b01fb3
Jacob Ashdown <jcbashdown@googlemail.com>
2013-05-17 13:46:33 +0100

    working location and id param!

--- app/assets/javascripts/angular_files/app.js
+++ app/assets/javascripts/angular_files/app.js
@@ -1,13 +1,13 @@
 'use strict';
 
 angular.module('centre', ["ngResource"]).
+  config(['$locationProvider', function($locationProvider) {    
+    $locationProvider.html5Mode(true);  
+  }]).
   config(['$routeProvider', function($routeProvider) {
-  $routeProvider.
-//this isn't loading, so controller directive no on page, so controller no called. 
-//undefined because not matching id
-    //alert($routeProvider);
-    when('/questions/:questionId', {templateUrl: '/assets/angular_files/templates/argument.html', controller: QuestionsCtrl}).
-    otherwise({redirectTo: '/'});
+    $routeProvider.
+      when('/questions/:questionId', {templateUrl: '/assets/angular_files/templates/argument.html', controller: QuestionsCtrl}).
+      otherwise({redirectTo: '/'});
   }]);
 
 
--- app/assets/javascripts/angular_files/controllers/questions_controller.js
+++ app/assets/javascripts/angular_files/controllers/questions_controller.js
@@ -2,7 +2,6 @@
 
 function QuestionsCtrl($scope, $routeParams, $http) {
   $http.get('/questions/1.json').success(function(data) {
-    alert($routeParams.questionId);
     $scope.question = data;
   });
 };
