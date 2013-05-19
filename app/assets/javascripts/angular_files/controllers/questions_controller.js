cba3b6c667ded704af54481666964b0144caeca3
Jacob Ashdown <jcbashdown@googlemail.com>
2013-05-20 00:30:07 +0100

    f

--- app/assets/javascripts/angular_files/controllers/questions_controller.js
+++ app/assets/javascripts/angular_files/controllers/questions_controller.js
@@ -6,6 +6,11 @@ function QuestionsCtrl($scope, $routeParams, $http) {
   });
   $scope.subPoint = function(node_id) {
     $http.get('/nodes/'+node_id+'.json').success(function(data) {
+      //http://www.cheatography.com/proloser/cheat-sheets/angularjs/
+      //get partial
+      //partial has controller declaration
+      //controller has nodes show
+      //nodes show called for node and partial scope done...
       return data; 
     });
   };
